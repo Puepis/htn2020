7849b8935204925e1866e3cd39c105bbdc2bdea5
Philips Xu <philips.ilips@gmail.com>
2021-01-17 04:05:57 -0500

    forgot import

--- app/routes/github.js
+++ app/routes/github.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const models = require("../models/cockroach");
 const { generateOcto } = require("../util/github");
 
 router.get("/listRepos", async (req, res) => {
