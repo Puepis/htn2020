5855e348897dd6c95a67db45641fe98fb2d4556c
Philips Xu <philips.ilips@gmail.com>
2021-01-17 05:54:46 -0500

    socket

--- app/index.js
+++ app/index.js
@@ -5,21 +5,7 @@ const userRouter = require("./routes/user");
 const models = require("./models/cockroach");
 const app = express();
 const http = require("http");
-const server = http.createServer(app);
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", (ws) => {
-  //connection is up, let's add a simple simple event
-  ws.on("message", (message) => {
-    //log the received message and send it back to the client
-    console.log("received: %s", message);
-    ws.send(`Hello, you sent -> ${message}`);
-  });
-
-  //send immediatly a feedback to the incoming connection
-  ws.send("Hi there, I am a WebSocket server");
-});
+const socketIO = require("socket.io");
 
 const cors = require("cors");
 
@@ -52,13 +38,15 @@ const PORT = process.env.PORT || 8000;
 models.sequelize
   .sync()
   .then(() => {
-    app.listen(PORT, () => {
+    var server = app.listen(PORT, () => {
       console.log(`App started on port ${PORT}`);
+    });
+
+    var io = socketIO(server);
+
+    io.on("connection", (socket) => {
+      console.log("Client connected");
+      socket.on("disconnect", () => console.log("Client disconnected"));
     });
   })
   .error((e) => console.error("error connecting to db: ", e));
-
-//start our server
-server.listen(3000, () => {
-  console.log(`Server started on port ${server.address().port} :)`);
-});
