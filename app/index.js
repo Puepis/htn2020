1695f6ea1cba206e4ed1482d9e3353d67397be80
Philips Xu <philips.ilips@gmail.com>
2021-01-17 05:42:38 -0500

    change

--- app/index.js
+++ app/index.js
@@ -4,15 +4,21 @@ const githubRouter = require("./routes/github");
 const userRouter = require("./routes/user");
 const models = require("./models/cockroach");
 const app = express();
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8080 });
+const http = require("http");
+const server = http.createServer(app);
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+wss.on("connection", (ws) => {
+  //connection is up, let's add a simple simple event
+  ws.on("message", (message) => {
+    //log the received message and send it back to the client
+    console.log("received: %s", message);
+    ws.send(`Hello, you sent -> ${message}`);
   });
 
-  ws.send('something');
+  //send immediatly a feedback to the incoming connection
+  ws.send("Hi there, I am a WebSocket server");
 });
 
 const cors = require("cors");
